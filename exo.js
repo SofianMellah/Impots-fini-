834165c950b29343605f2e3f8d6a7394c3e5dd66
59013-97-11 <59013-97-11@exchange.ad.afpanet>
2023-02-28 11:56:57 +0100

    egeg

--- exo.js
+++ exo.js
@@ -2,13 +2,13 @@ function impots(){
     let age = document.getElementById('age').value
     let sexe = document.getElementById('sexe').value
     let imposable = ( age >= 20 && (sexe ==='Homme' || sexe ==='homme')) || (age >= 18 && age <= 35 && (sexe === 'Femme' || sexe === 'femme'))
-    let nonimposable = ( age < 20 && (sexe === 'Homme' || sexe === 'homme')) || ((age < 18 || age > 35) && (sexe === 'Femme' || sexe === 'femme'))
+    let nonImposable = ( age < 20 && (sexe === 'Homme' || sexe === 'homme')) || ((age < 18 || age > 35) && (sexe === 'Femme' || sexe === 'femme'))
     console.log(age);
     console.log(sexe);
 if (imposable){
     console.log(`Vous êtes imposable`)
     document.getElementById('input3').value = 'Vous êtes imposable'
-} else if (nonimposable){
+} else if (nonImposable){
     console.log(`Vous êtes non imposable`)
     document.getElementById('input3').value = 'Vous êtes non imposable'
 } else{         
